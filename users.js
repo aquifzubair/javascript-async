6146349a5c2c64d6095abfebe85e002d37b6fe3b
aquifzubair2 <aquif.zunair@mountblue.tech>
2020-07-24 11:19:52 +0530

    implemented functions to send a login request to user1, fetch profile and post of user1 serially as well as parallaly. Changed all functions to arrow functions

--- users.js
+++ users.js
@@ -1,59 +1,85 @@
-function fetchData(data) {
-    return new Promise(resolve => setTimeout(() => {
-      resolve(data)
-    }, 1000))
-}
+const fetchData = (data) => {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(data);
+    }, 1000)
+  );
+};
 
-function sendUserLoginRequest(userId){
-    console.log(`Sending login request for ${userId}...`);
-    return fetchData(userId).then(() => {
-        console.log('Login successfull.', userId);
-        return userId;
-    })
-}
+const sendUserLoginRequest = (userId) => {
+  console.log(`Sending login request for ${userId}...`);
+  return fetchData(userId).then(() => {
+    console.log("Login successfull.", userId);
+    return userId;
+  });
+};
 
-function getUserProfile(userId){
-    console.log(`Fetching profile for ${userId}...`);
-    return fetchData({
-        user1: {name: 'Vijay', points: 100},
-        user2: {name: 'Sahana', points: 200}
-    }).then(profiles => {
-        console.log(`Received profile for ${userId}`);
-        return profiles[userId];
-    });
-}
+const getUserProfile = (userId) => {
+  console.log(`Fetching profile for ${userId}...`);
+  return fetchData({
+    user1: { name: "Vijay", points: 100 },
+    user2: { name: "Sahana", points: 200 },
+  }).then((profiles) => {
+    console.log(`Received profile for ${userId}`);
+    return profiles[userId];
+  });
+};
 
-function getUserPosts(userId){
-    console.log(`Fetching posts for ${userId}...`);
-    return fetchData({
-        user1: [{id: 1, title: 'Economics 101'}, {id: 2, title: 'How to negotiate'}],
-        user2: [{id: 3, title: 'CSS Animations'}, {id: 4, title: 'Understanding event loop'}]
-    }).then(posts => {
-        console.log(`Received posts for ${userId}`);
-        return posts[userId];
-    });
-}
+const getUserPosts = (userId) => {
+  console.log(`Fetching posts for ${userId}...`);
+  return fetchData({
+    user1: [
+      { id: 1, title: "Economics 101" },
+      { id: 2, title: "How to negotiate" },
+    ],
+    user2: [
+      { id: 3, title: "CSS Animations" },
+      { id: 4, title: "Understanding event loop" },
+    ],
+  }).then((posts) => {
+    console.log(`Received posts for ${userId}`);
+    return posts[userId];
+  });
+};
 
+// **
+//  * Task 1: Send a login request for user1 -> get user profile data -> get user posts data
+//  */
 
-/**
- * Task 1: Send a login request for user1 -> get user profile data -> get user posts data
- */
-
-function userDataSerial(){
-  console.time('userData-serial');
+const userDataSerial = async () => {
+  console.time("userData-serial");
   // Write code here
-  console.timeEnd('userData-serial');
-}
+  const userId = await sendUserLoginRequest("user1");
+
+  const userProfile = await getUserProfile(userId);
+  console.log("User Profile", userProfile);
+
+  const userPosts = await getUserPosts(userId);
+  console.log("user posts", userPosts);
+
+  console.timeEnd("userData-serial");
+};
 
 /**
  * Task 2: Send a login request for user1 -> get user profile data and get user posts data simultaneously
  */
 
-function userDataParallel() {
-  console.time('userData-parallel');
+const userDataParallel = async () => {
+  console.time("userData-parallel");
   // Write code here
-  console.timeEnd('userData-parallel');
-}
+  const userId = await sendUserLoginRequest("user1");
+
+  const profileAndPost = await Promise.all([
+    getUserProfile(userId),
+    getUserPosts(userId),
+  ]);
+
+  const [profile, posts] = profileAndPost;
+  console.log("Profile of User1 ", profile);
+  console.log("Posts of User1 ", posts);
+
+  console.timeEnd("userData-parallel");
+};
 
 userDataSerial();
 userDataParallel();
