dfe505b5d552c3690db20809c5616a915f1b7079
aquifzubair2 <aquif.zunair@mountblue.tech>
2020-07-24 13:40:48 +0530

    refactor task 3 in random-solution file. pushed all promises in an array and passed it in promise.all

--- solutions/random-solution.js
+++ solutions/random-solution.js
@@ -25,57 +25,68 @@ const fetchRandomString = () => {
 // **Task 1**: Right now, the function fetchRandomNumbers can be used by passing a callback,
 // Your task is to promisfy this function so that the following can be done:
 
-fetchRandomNumbers((randomNum) => console.log(randomNum))
-.catch((err) =>
-  console.error(err)
-);
+// fetchRandomNumbers((randomNum) => console.log(randomNum))
+// .catch((err) =>
+//   console.error(err)
+// );
 
-fetchRandomString((randomStr) => console.log(randomStr))
-.catch((err) =>
-  console.error(err)
-);
+// fetchRandomString((randomStr) => console.log(randomStr))
+// .catch((err) =>
+//   console.error(err)
+// );
 
-// **Task 2**: Fetch a random number -> add it to a sum variable and print sum-> fetch another random variable
-// -> add it to the same sum variable and print the sum variable.
+// // **Task 2**: Fetch a random number -> add it to a sum variable and print sum-> fetch another random variable
+// // -> add it to the same sum variable and print the sum variable.
 
-fetchRandomNumbers((randomNum) => randomNum)
-  .then((randomNum) => {
-    let sum = 5;
-    console.log("Sum of first random number and sum", sum + randomNum);
-    return sum;
-  })
-  .then((sum) => {
-    return fetchRandomNumbers((randomNum) => randomNum).then(
-      (randomNum) => randomNum + sum
-    )
-    .catch(error => console.error(error));
-  })
-  .then((sum) => console.log("Sum of 2nd random number and sum", sum))
-  .catch((err) => console.error(err));
+// fetchRandomNumbers((randomNum) => randomNum)
+//   .then((randomNum) => {
+//     let sum = 5;
+//     console.log("Sum of first random number and sum", sum + randomNum);
+//     return sum;
+//   })
+//   .then((sum) => {
+//     return fetchRandomNumbers((randomNum) => randomNum).then(
+//       (randomNum) => randomNum + sum
+//     )
+//     .catch(error => console.error(error));
+//   })
+//   .then((sum) => console.log("Sum of 2nd random number and sum", sum))
+//   .catch((err) => console.error(err));
 
-// **Task 3**: Fetch a random number and a random string simultaneously, concatenate their
-// and print the concatenated string
+// // **Task 3**: Fetch a random number and a random string simultaneously, concatenate their
+// // and print the concatenated string
 
-Promise.all([fetchRandomNumbers(), fetchRandomString()])
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err));
+// Promise.all([fetchRandomNumbers(), fetchRandomString()])
+//   .then((data) => console.log(data))
+//   .catch((err) => console.error(err));
 
 // **Task 4**: Fetch 10 random numbers simultaneously -> and print their sum.
 
-Promise.all([
-  fetchRandomNumbers(),
-  fetchRandomNumbers(),
-  fetchRandomNumbers(),
-  fetchRandomNumbers(),
-  fetchRandomNumbers(),
-  fetchRandomNumbers(),
-  fetchRandomNumbers(),
-  fetchRandomNumbers(),
-  fetchRandomNumbers(),
-  fetchRandomNumbers(),
-])
-  .then((value) => {
-    return value.reduce((acc, curr) => acc + curr);
+// Promise.all([
+//   fetchRandomNumbers(),
+//   fetchRandomNumbers(),
+//   fetchRandomNumbers(),
+//   fetchRandomNumbers(),
+//   fetchRandomNumbers(),
+//   fetchRandomNumbers(),
+//   fetchRandomNumbers(),
+//   fetchRandomNumbers(),
+//   fetchRandomNumbers(),
+//   fetchRandomNumbers(),
+// ])
+//   .then((value) => {
+//     return value.reduce((acc, curr) => acc + curr);
+//   })
+//   .then((val) => console.log(val))
+//   .catch((err) => console.error(err));
+let promiseArray = [];
+for (let i = 0; i < 10; i++) {
+  promiseArray.push(fetchRandomNumbers());
+}
+let array = Promise.all(promiseArray);
+array
+  .then((data) => {
+    const sum = data.reduce((acc, curr) => acc + curr);
+    return sum;
   })
-  .then((val) => console.log(val))
-  .catch((err) => console.error(err));
+  .then((sum) => console.log(sum));
